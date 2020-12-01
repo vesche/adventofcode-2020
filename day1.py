7c45e7ec65566fcce33ae151bed8c179f3b91840
vesche <vesche@protonmail.com>
2020-12-01 12:00:33 -0600

    day 1

---
+++ day1.py
@@ -0,0 +1,18 @@
+with open('input1.txt') as f:
+    data = list(map(int, f.read().splitlines()))
+
+def part1():
+    for a in data:
+        for b in data:
+            if a + b == 2020:
+                return f'part 1: {a * b}'
+
+def part2():
+    for a in data:
+        for b in data:
+            for c in data:
+                if a + b + c == 2020:
+                    return f'part 2: {a * b * c}'
+
+print(part1())
+print(part2())
