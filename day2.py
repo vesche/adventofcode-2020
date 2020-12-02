6ea4d33779bb3841d4c4a53253184f0ab34fe737
vesche <vesche@protonmail.com>
2020-12-01 23:29:11 -0600

    day 2

---
+++ day2.py
@@ -0,0 +1,19 @@
+with open('input2.txt') as f:
+    data = f.read().splitlines()
+
+part1, part2 = 0, 0
+
+for line in data:
+    n, l, password = line.split()
+    n1, n2 = map(int, n.split('-'))
+    letter = l.rstrip(':')
+
+    if n1 <= password.count(letter) <= n2:
+        part1 += 1
+
+    s = password[n1-1] + password[n2-1]
+    if s.count(letter) == 1:
+        part2 += 1
+
+print(part1)
+print(part2)
