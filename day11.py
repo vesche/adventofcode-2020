24cbf75c80a639df4085ee44c9d5150685d5eb24
vesche <vesche@protonmail.com>
2020-12-11 00:29:25 -0600

    day 11

--- day11.py
+++ day11.py
@@ -1,44 +1,56 @@
 with open('input11.txt') as f:
-    data = f.read().splitlines()
+    grid = [list(i) for i in f.read().splitlines()]
 
-foo = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+grid_b = [_[:] for _ in grid]
+coords = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-new_data = [['']*len(data[0]) for i in range(len(data))]
+def find_occ_p1(grid, i, j):
+    x, l, c = 0, len(grid), len(grid[0])
+    for a, b in coords:
+        ia, jb = i+a, j+b
+        if 0 <= ia < l and 0 <= jb < c and grid[ia][jb] == '#':
+            x += 1
+    return x
 
-x = 0
+def find_occ_p2(grid, i, j):
+    x, l, c = 0, len(grid), len(grid[0])
+    for a, b in coords:
+        ia, jb = i+a, j+b
+        while 0 <= ia < l and 0 <= jb < c:
+            if grid[ia][jb] == '#':
+                x += 1; break
+            elif grid[ia][jb] == 'L':
+                break
+            ia += a
+            jb += b
+    return x
 
-while True:
-    if x:
-        data2 = new_data[:]
-    else:
-        data2 = data
-    x += 1
-    new_data = [['']*len(data[0]) for i in range(len(data))]
+def solve(p1=True):
+    while True:
+        same, grid_copy = True, [_[:] for _ in grid]
+        for i, row in enumerate(grid_copy):
+            for j, col in enumerate(row):
+                if p1:
+                    n = find_occ_p1(grid_copy, i, j)
+                    nx = 4
+                else:
+                    n = find_occ_p2(grid_copy, i, j)
+                    nx = 5
+                if col == 'L' and n == 0:
+                    grid[i][j] = '#'
 
-    for i, line in enumerate(data):
-        for j, seat in enumerate(line):
-            if seat == '.':
-                new_data[i][j] = '.'
-                continue
+                elif col == '#' and n >= nx:
+                    grid[i][j] = 'L'
+                same &= row[j] == grid[i][j]
+        if same:
+            break
 
-            around = list()
-            for a, b in foo:
-                try:
-                    y, x = i+a, j+b
-                    if (x < 0 or y < 0):
-                        continue
-                    around.append(data2[y][x])
-                except:
-                    pass
+def count_occ(grid):
+    return ''.join([''.join(i) for i in grid]).count('#')
 
-            if set(around) == {'L', '.'} or set(around) == {'L'}:
-                new_data[i][j] = '#'
-            elif seat == '#' and around.count('#') >= 4:
-                new_data[i][j] = 'L'
-            else:
-                new_data[i][j] = seat
+solve()
+print(count_occ(grid))
 
-    if new_data == data2:
-        break
-
-print(''.join([''.join(i) for i in new_data]).count('#'))
+grid = grid_b
+solve(p1=False)
+print(count_occ(grid))
